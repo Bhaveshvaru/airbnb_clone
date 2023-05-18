f562b4b6780b7280d0204a92627b192edfb7ae2e
Bhavesh Varu <bhavesh.varu97@gmail.com>
2023-05-18 23:06:40 +0800

    updated icon component

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import design from '../../assets/design.svg'
 import beach from '../../assets/beach.svg'
 import castle from '../../assets/castle.svg'
@@ -11,41 +10,31 @@ import trending from '../../assets/trending.svg'
 import building from '../../assets/building.svg'
 import water from '../../assets/water.svg'
 import camping from '../../assets/camping.svg'
-
 import './icon.css'
 
-const arrIcon = []
-arrIcon.push(
-  design,
-  beach,
-  castle,
-  window,
-  city,
-  views,
-  single_home,
-  trending,
-  building,
-  water,
-  camping
-)
-
-// let iconObj=[
-// {title:'Design'}
-// {title:'Beach'}
-// {title:'Castle'}
-// {title:'Amazing View'}
-// {title:''}
-// ]
+let iconObj = [
+  { title: 'Design', icon: design },
+  { title: 'Beach', icon: beach },
+  { title: 'Castle', icon: castle },
+  { title: 'Amazing View', icon: window },
+  { title: 'Luxe', icon: city },
+  { title: 'Pool', icon: views },
+  { title: 'Cabins', icon: single_home },
+  { title: 'Trending', icon: trending },
+  { title: 'Cities', icon: building },
+  { title: 'Island', icon: water },
+  { title: 'Camping', icon: camping },
+]
 
 const icon = () => {
   return (
     <div className='icon_list'>
-      {arrIcon.map((item, index) => {
+      {iconObj.map((item) => {
         return (
-          <div key={index}>
+          <div key={item.icon}>
             <div className='list' key={item}>
-              <img width='25px' height='25px' src={`${item}`} />
-              <p className='title'>Camping</p>
+              <img width='25px' height='25px' src={`${item.icon}`} />
+              <p className='title'>{item.title}</p>
               <hr className='icon_hr' />
             </div>
           </div>
